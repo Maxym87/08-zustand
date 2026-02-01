baa0cdfdd30fc26da25700c8cb29b76f0f543f53
Maks Shkafar <m.shkafar@gmail.com>
2026-02-01 12:21:41 +0100

    Add metadata for not-found page

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,5 +1,25 @@
 import Link from "next/link";
 import css from "./Home.module.css";
+import { Metadata } from "next";
+import { url } from "inspector";
+
+export const metadata: Metadata = {
+  title: "Page not found",
+  description: "The page you are looking for does not exist on NoteHub.",
+  openGraph: {
+    title: "Page not found",
+    description: "The page you are looking for does not exist on NoteHub.",
+    url: "https://08-zustand-chi-three.vercel.app/not-found",
+    images: [
+      {
+        url: "https://ac.goit.global/fullstack/react/notehub-og-meta.jpg",
+        width: 1200,
+        height: 630,
+        alt: "NoteHub - Page Not Found",
+      },
+    ],
+  },
+};
 
 const NotFound = () => {
   return (
